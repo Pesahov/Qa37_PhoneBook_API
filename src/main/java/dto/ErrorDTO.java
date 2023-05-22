351fbd18f21ad38bccccba28cbd275131001afd4
Shilol Pesahov <pesahov777@gmail.com>
2023-05-22 19:56:28 +0300

    create login class

---
+++ src/main/java/dto/ErrorDTO.java
@@ -0,0 +1,18 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+
+public class ErrorDTO {
+    private String error;
+    private int status;
+    private String path;
+    private Object message;
+}
